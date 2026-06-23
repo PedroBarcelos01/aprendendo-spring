be803cc537ad0cfdf6b4936302bb8c2f1ee530e3
Pedro Barcelos <pedromiguel1203@hotmail.com>
2026-06-23 20:55:12 -0300

    Primeira versão do meu projeto

---
+++ src/main/java/com/pedromiguel/aprendendospring/infrastructure/exceptions/ConflictException.java
@@ -0,0 +1,11 @@
+package com.pedromiguel.aprendendospring.infrastructure.exceptions;
+
+public class ConflictException extends RuntimeException {
+    public ConflictException(String mensagem) {
+        super(mensagem);
+    }
+
+    public ConflictException(String mensagem, Throwable throwable) {
+        super(mensagem);
+    }
+}
